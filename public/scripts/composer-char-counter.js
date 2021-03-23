94e1603a05a7e13b5df1f2805de8cdc656067b88
Devin MacGillivray <devhmac@gmail.com>
2021-03-23 19:08:55 +0000

    char counter counts down & turns red if < 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,21 @@
 $(document).ready(function() {
   console.log('The document is ready!')
 
-  $('#tweet-text').on('keypress', function(e) {
-    let charCount = this.value.length;
-    console.log(140 - charCount)
-  })
-})
+  $('#tweet-text').on('input', function(e) {
+    let charCount = $(this).val().length;
+    let remainingChars = 140 - charCount;
+
+    //dom traversal targeting of .counter value
+    let counter = $(this).parent().next('div').children('.counter')
+
+    counter.text(remainingChars)
+
+    //selector method of targerting .counter value
+    //$('.counter').val(remainingChars);
+    if (remainingChars < 0) {
+      counter.addClass('redText')
+    } else {
+      counter.removeClass('redText')
+    }
+  });
+});
