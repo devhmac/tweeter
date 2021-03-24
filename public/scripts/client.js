b0c14584502c06d8a61f8be957005c677be4ad54
Devin MacGillivray <devhmac@gmail.com>
2021-03-24 02:01:50 +0000

    completes renderTweet logic

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,17 +5,31 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-}
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 
 const createTweetElement = function(data) {
   return (`
@@ -44,13 +58,17 @@ const createTweetElement = function(data) {
   `);
 };
 
-const $tweet = createTweetElement(tweetData)
+const renderTweet = function(data) {
+  for (let tweet of data) {
+    let tweetTemp = createTweetElement(tweet);
+    $('#tweets-container').append(tweetTemp);
+  }
+}
+
+
 
 
 $(document).ready(function() {
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
+  renderTweet(data);
 
 });
