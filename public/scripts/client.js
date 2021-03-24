cabc2d25644532383934e835a4404e576611a099
Devin MacGillivray <devhmac@gmail.com>
2021-03-24 01:37:53 +0000

    building template literal for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,53 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+    "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
+
+const createTweetElement = function(data) {
+  return (`
+  <article class="tweet">
+  <header>
+    <div class="user">
+      <img
+        src="${data.user.avatars}"
+        alt="">
+      <p>${data.user.name}</p>
+    </div>
+    <h4>${data.user.handle}</h4>
+  </header>
+
+  <p>${data.content.text}</p>
+
+  <footer>
+    <span>${data.created_at}</span>
+    <div>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+    </div>
+  </footer>
+</article>
+  `);
+};
+
+const $tweet = createTweetElement(tweetData)
+
+
+$(document).ready(function() {
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+});
